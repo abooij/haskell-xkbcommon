bec6968b40cf9d87644541c7593f5dfefbe0f314
Auke Booij <auke@tulcod.com>
2013-07-23 23:27:17 +0200

    minor fixes in ParseDefines

--- Text/XkbCommon/ParseDefines.hs
+++ Text/XkbCommon/ParseDefines.hs
@@ -20,5 +20,8 @@ genKeysyms :: IO [Dec]
 genKeysyms = do
    (filename,keysyms_header) <- readHeader "xkbcommon/xkbcommon-keysyms.h"
    (_,defs) <- macroPassReturningSymTab [] defaultBoolOptions [(newfile filename,keysyms_header)]
-   return $ map (\ (name, val) -> ValD (VarP $ mkName ("xkb_key"++drop 7 name)) (NormalB (AppE (ConE $ mkName "CKeysym") $ LitE (IntegerL (read val)))) []) $ filter ((/="").snd) defs
+   let exclude_defs = ["XKB_KEY_VoidSymbol", "XKB_KEY_NoSymbol"]
+   let filtered_defs = filter (\ (name,val) -> isPrefixOf "XKB_KEY" name && notElem name exclude_defs) defs
+   let parsed_defs = map (\ (name, val) -> (drop 7 name, read val)) filtered_defs
+   return $ map (\ (name, val) -> ValD (VarP $ mkName ("xkb_key"++name)) (NormalB (AppE (ConE $ mkName "CKeysym") $ LitE (IntegerL val))) []) parsed_defs
 
