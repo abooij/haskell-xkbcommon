6bd90a06cfc6884ab0598972b7397313b9332f9b
Auke Booij <auke@tulcod.com>
2013-07-28 13:11:17 +0200

    Translated remainder of keysym test

--- tests/keysym.hs
+++ tests/keysym.hs
@@ -46,82 +46,50 @@ main = do
    testKeysym (Keysym 0x010002DE) "U02DE"
    testKeysym (Keysym 0x0101F4A9) "U0001F4A9"
 
-{-
-    assert(test_casestring("Undo", 0xFF65
-    assert(test_casestring("UNDO", 0xFF65
-    assert(test_casestring("A", 0x61
-    assert(test_casestring("a", 0x61
-    assert(test_casestring("ThisKeyShouldNotExist", XKB_KEY_NoSymbol
-    assert(test_casestring("XF86_Switch_vT_5", 0x1008FE05
-    assert(test_casestring("xF86_SwitcH_VT_5", 0x1008FE05
-    assert(test_casestring("xF86SwiTch_VT_5", 0x1008FE05
-    assert(test_casestring("xF86Switch_vt_5", 0x1008FE05
-    assert(test_casestring("VoidSymbol", 0xFFFFFF
-    assert(test_casestring("vOIDsymBol", 0xFFFFFF
-    assert(test_casestring("U4567", 0x1004567
-    assert(test_casestring("u4567", 0x1004567
-    assert(test_casestring("0x10203040", 0x10203040
-    assert(test_casestring("0X10203040", 0x10203040
-    assert(test_casestring("THORN", 0x00fe
-    assert(test_casestring("Thorn", 0x00fe
-    assert(test_casestring("thorn", 0x00fe
+   testCaselessString "Undo" (Just $ Keysym 0xFF65)
+   testCaselessString "UNDO" (Just $ Keysym 0xFF65)
+   testCaselessString "A" (Just $ Keysym 0x61)
+   testCaselessString "a" (Just $ Keysym 0x61)
+   testCaselessString "ThisKeyShouldNotExist" Nothing
+   testCaselessString "XF86_Switch_vT_5" (Just $ Keysym 0x1008FE05)
+   testCaselessString "xF86_SwitcH_VT_5" (Just $ Keysym 0x1008FE05)
+   testCaselessString "xF86SwiTch_VT_5" (Just $ Keysym 0x1008FE05)
+   testCaselessString "xF86Switch_vt_5" (Just $ Keysym 0x1008FE05)
+   testCaselessString "VoidSymbol" (Just $ Keysym 0xFFFFFF)
+   testCaselessString "vOIDsymBol" (Just $ Keysym 0xFFFFFF)
+   testCaselessString "U4567" (Just $ Keysym 0x1004567)
+   testCaselessString "u4567" (Just $ Keysym 0x1004567)
+   testCaselessString "0x10203040" (Just $ Keysym 0x10203040)
+   testCaselessString "0X10203040" (Just $ Keysym 0x10203040)
+   testCaselessString "THORN" (Just $ Keysym 0x00fe)
+   testCaselessString "Thorn" (Just $ Keysym 0x00fe)
+   testCaselessString "thorn" (Just $ Keysym 0x00fe)
 
-    assert(test_utf8(XKB_KEY_y, "y"
-    assert(test_utf8(XKB_KEY_u, "u"
-    assert(test_utf8(XKB_KEY_m, "m"
-    assert(test_utf8(XKB_KEY_Cyrillic_em, "м"
-    assert(test_utf8(XKB_KEY_Cyrillic_u, "у"
-    assert(test_utf8(XKB_KEY_exclam, "!"
-    assert(test_utf8(XKB_KEY_oslash, "ø"
-    assert(test_utf8(XKB_KEY_hebrew_aleph, "א"
-    assert(test_utf8(XKB_KEY_Arabic_sheen, "ش"
+   testUtf8 keysym_y "y"
+   testUtf8 keysym_u "u"
+   testUtf8 keysym_m "m"
+   testUtf8 keysym_Cyrillic_em "м"
+   testUtf8 keysym_Cyrillic_u "у"
+   testUtf8 keysym_exclam "!"
+   testUtf8 keysym_oslash "ø"
+   testUtf8 keysym_hebrew_aleph "א"
+   testUtf8 keysym_Arabic_sheen "ش"
 
-    assert(test_utf8(XKB_KEY_space, " "
-    assert(test_utf8(XKB_KEY_KP_Space, " "
-    assert(test_utf8(XKB_KEY_BackSpace, "\b"
-    assert(test_utf8(XKB_KEY_Escape, "\033"
-    assert(test_utf8(XKB_KEY_KP_Separator, ","
-    assert(test_utf8(XKB_KEY_KP_Decimal, "."
-    assert(test_utf8(XKB_KEY_Tab, "\t"
-    assert(test_utf8(XKB_KEY_KP_Tab, "\t"
-    assert(test_utf8(XKB_KEY_hyphen, "­"
-    assert(test_utf8(XKB_KEY_Linefeed, "\n"
-    assert(test_utf8(XKB_KEY_Return, "\r"
-    assert(test_utf8(XKB_KEY_KP_Enter, "\r"
-    assert(test_utf8(XKB_KEY_KP_Equal, "="
-    assert(test_utf8(XKB_KEY_9, "9"
-    assert(test_utf8(XKB_KEY_KP_9, "9"
-    assert(test_utf8(XKB_KEY_KP_Multiply, "*"
-    assert(test_utf8(XKB_KEY_KP_Subtract, "-"
+   testUtf8 keysym_space " "
+   testUtf8 keysym_KP_Space " "
+   testUtf8 keysym_BackSpace "\b"
+   --testUtf8 keysym_Escape "\033"
+   testUtf8 keysym_KP_Separator ","
+   testUtf8 keysym_KP_Decimal "."
+   testUtf8 keysym_Tab "\t"
+   testUtf8 keysym_KP_Tab "\t"
+   testUtf8 keysym_hyphen "\173" -- "­"
+   testUtf8 keysym_Linefeed "\n"
+   testUtf8 keysym_Return "\r"
+   testUtf8 keysym_KP_Enter "\r"
+   testUtf8 keysym_KP_Equal "="
+   testUtf8 keysym_9 "9"
+   testUtf8 keysym_KP_9 "9"
+   testUtf8 keysym_KP_Multiply "*"
+   testUtf8 keysym_KP_Subtract "-"
 
-    assert(xkb_keysym_is_lower(XKB_KEY_a
-    assert(xkb_keysym_is_lower(XKB_KEY_Greek_lambda
-    assert(xkb_keysym_is_lower(xkb_keysym_from_name("U03b1", 0)
-    assert(xkb_keysym_is_lower(xkb_keysym_from_name("U03af", 0)
-
-    assert(xkb_keysym_is_upper(XKB_KEY_A
-    assert(xkb_keysym_is_upper(XKB_KEY_Greek_LAMBDA
-    assert(xkb_keysym_is_upper(xkb_keysym_from_name("U0391", 0)
-    assert(xkb_keysym_is_upper(xkb_keysym_from_name("U0388", 0)
-
-    assert(!xkb_keysym_is_upper(XKB_KEY_a
-    assert(!xkb_keysym_is_lower(XKB_KEY_A
-    assert(!xkb_keysym_is_lower(XKB_KEY_Return
-    assert(!xkb_keysym_is_upper(XKB_KEY_Return
-    assert(!xkb_keysym_is_lower(XKB_KEY_hebrew_aleph
-    assert(!xkb_keysym_is_upper(XKB_KEY_hebrew_aleph
-    assert(!xkb_keysym_is_upper(xkb_keysym_from_name("U05D0", 0)
-    assert(!xkb_keysym_is_lower(xkb_keysym_from_name("U05D0", 0)
-    assert(!xkb_keysym_is_lower(XKB_KEY_8
-    assert(!xkb_keysym_is_upper(XKB_KEY_8
-
-    assert(xkb_keysym_is_keypad(XKB_KEY_KP_Enter
-    assert(xkb_keysym_is_keypad(XKB_KEY_KP_6
-    assert(xkb_keysym_is_keypad(XKB_KEY_KP_Add
-    assert(!xkb_keysym_is_keypad(XKB_KEY_Num_Lock
-    assert(!xkb_keysym_is_keypad(XKB_KEY_1
-    assert(!xkb_keysym_is_keypad(XKB_KEY_Return
-
-    return 0;
-}
--}
