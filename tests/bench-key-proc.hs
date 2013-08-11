1256b084c2d65c3e428d12b5e5d242ee63ac57b9
Auke Booij <auke@tulcod.com>
2013-08-11 23:37:20 +0200

    (re)-added benchmark to project file

--- tests/bench-key-proc.hs
+++ tests/bench-key-proc.hs
@@ -1,7 +1,8 @@
 import Data.Maybe
-import Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed as V
 import Control.Monad
 import System.Random
+import Data.Time.Clock
 
 import Text.XkbCommon
 
@@ -13,20 +14,26 @@ benchmarkIterations = 20000000
 --updateHead a xs = a:V.tail xs
 --update i a xs = V.take i xs ++ updateHead a (V.drop i xs)
 
-update i a xs = xs // [(i,a)]
+update i a xs = xs V.// [(i,a)]
 
-bench :: KeymapState -> Int -> Vector Bool -> IO ()
+bench :: KeymapState -> Int -> V.Vector Bool -> IO ()
 bench st n keys = unless (n == 0) $ do
    rand <- getStdRandom (randomR (9,255))
    updateKeymapState st (CKeycode $ fromIntegral rand)
-      (if keys ! rand then keyUp else keyDown)
+      (if keys V.! rand then keyUp else keyDown)
    return =<< bench st (n-1) $ Main.update rand (not $ keys V.! rand) keys  -- keys // [(rand,(not $ keys V.! rand))]
 
 main = do
+   start <- getCurrentTime
+
    ctx <- getTestContext
    km <- liftM fromJust $ newKeymapFromNames ctx (RMLVO (Just "evdev") (Just "pc104") (Just "us,ru,il,de") (Just ",,,neo") (Just "grp:menu_toggle"))
    st <- newKeymapState km
 
    bench st benchmarkIterations (V.replicate 256 False)
+
+   end <- getCurrentTime
+
+   putStrLn $ "bench-key-proc took " ++ show (diffUTCTime end start)
 
    return ()
